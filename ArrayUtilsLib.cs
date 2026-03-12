4a8742251aec2b520a3b6da5d542114faf5ee012
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-03-11 21:14:23 -0400

    feat: created GetTheMax from the an array

---
+++ ArrayUtilsLib.cs
@@ -0,0 +1,20 @@
+﻿namespace ArrayUtilsLib
+{
+    public class ArrayUtilsLib
+    {
+        public static int GetTheMax(int[] in_array)
+        {
+            int max = 0;
+
+            foreach(var item in in_array)
+            {
+                if(item > max)
+                {
+                    max = item;
+                }
+            }
+
+            return max;
+        }
+    }
+}
--- Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ArrayUtilsLib
-{
-    public class Class1
-    {
-
-    }
-}
